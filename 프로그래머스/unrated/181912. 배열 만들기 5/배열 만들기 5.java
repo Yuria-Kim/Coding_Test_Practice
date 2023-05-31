c893135ec4d4a69e5737fb603ee63ba9e7258c08
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-01 00:55:31 +0900

    Update 배열 만들기 5.java

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,7 +1,28 @@
 import java.util.*;
 
+// class Solution {
+//     public int[] solution(String[] intStrs, int k, int s, int l) {
+//         return Arrays.stream(intStrs).mapToInt(value -> Integer.parseInt(value.substring(s, s + l))).filter(value -> value > k).toArray();
+//     }
+// }
+
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        return Arrays.stream(intStrs).mapToInt(value -> Integer.parseInt(value.substring(s, s + l))).filter(value -> value > k).toArray();
+        List<Integer> answerList = new ArrayList<>();
+        int rev[] = new int[intStrs.length];
+        for(int i = 0; i<intStrs.length; i++){
+            rev[i]= Integer.valueOf(intStrs[i].substring(s, s+l));
+            if(rev[i]>k){
+                answerList.add(rev[i]);
+            }
+        }
+        
+        // ArrayList를 배열로 변환
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+        
+        return answer;
     }
 }
