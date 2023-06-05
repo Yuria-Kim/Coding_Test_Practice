6c890715a8b445ad80294131059b829d46dc17bf
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-05 22:31:42 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.73 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : my_string.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                result.append(Character.toLowerCase(ch));
+            } 
+            else if (Character.isLowerCase(ch)) {
+                result.append(Character.toUpperCase(ch));
+            } 
+        }
+        return result.toString();
+    }
+}
