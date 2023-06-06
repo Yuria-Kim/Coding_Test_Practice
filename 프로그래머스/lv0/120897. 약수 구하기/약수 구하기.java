4f0cb250484881c9354a77af76074393a6191200
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-06 19:33:07 +0900

    [level 0] Title: 약수 구하기, Time: 3.53 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -1,17 +1,8 @@
-import java.util.*;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int n) {
-        List<Integer> measure = new ArrayList<>();
-        for(int i = 1; i<=n; i++){
-            if(n%i ==0){
-                measure.add(i);
-            }
-        }
-        int[] answer = new int[measure.size()];
-        for(int i = 0; i<answer.length; i++){
-            answer[i] = measure.get(i);
-        }
-        Arrays.sort(answer);
-        return answer;
+        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).toArray();
     }
 }
