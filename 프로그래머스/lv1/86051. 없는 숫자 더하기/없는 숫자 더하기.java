f7100a9a4a428d5556b0d05e7d9e56d65fd59c80
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-10 22:54:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,17 +1,9 @@
 import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int compare[] = new int[10];
-        Arrays.sort(numbers);
-        for(int i = 0; i<numbers.length; i++){
-            int temp = numbers[i];
-            compare[temp]++;
-        }
-        for(int i = 0; i<compare.length; i++){
-            if(compare[i] == 0){
-                answer+=i;
-            }
+        int answer = 45;
+        for(int i : numbers){
+            answer -=i;
         }
         return answer;
     }
