fee27df1e2fba96e697f4d90b14105259d5c8119
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-11 19:50:26 +0900

    [unrated] Title: 문자열 묶기, Time: 4.52 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -1,16 +1,14 @@
+import java.util.*;
 class Solution {
     public int solution(String[] strArr) {
         
         int cnt [] = new int [31];
         for(int i = 0; i<strArr.length; i++){
-            int temp = strArr[i].length();
-            cnt[temp]++;
+            cnt[strArr[i].length()]++;
         }
         int answer = 0;
         for(int i = 1; i<cnt.length; i++){
-            if(cnt[i]>answer){
-                answer = cnt[i];
-            }
+            answer = Math.max(answer, cnt[i]);
         }
         return answer;
     }
