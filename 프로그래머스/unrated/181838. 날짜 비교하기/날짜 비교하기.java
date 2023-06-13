1f1cac583a49896d5c89a1534e5ed16ebeb0e773
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-13 19:56:02 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.04 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,14 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        int answer = 0;
-        String d1 = "";
-        String d2 = "";
-        for(int i = 0; i<date1.length; i++) {
-            d1 += ""+date1[i];
-            d2 += ""+date2[i];
-		}
-        int result = Integer.valueOf(d1)-Integer.valueOf(d2);
-        answer = (result>=0)?0:1;
-        return answer;
+        return Arrays.compare(date1, date2) < 0 ? 1 : 0;
     }
 }
