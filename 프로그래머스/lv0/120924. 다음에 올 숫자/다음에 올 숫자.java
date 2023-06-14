160d411d469d1474e7f7e11834062c000e2b63ed
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-14 20:32:11 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,29 +1,16 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        boolean diff = false;
-        //등비인지 등차인지 확인
-        //공차 구하기
-        //답 찾기
-        //일단, 등차로 가정
-        int t_arr [] = new int[common.length/2+1];
-        int k = 0;
-        int temp = common[1]-common[0];
-        for(int i = 2; i<common.length; i++){
-            k = common[i] - common[i-1];
-            if (k==temp){
-                diff = true;
-                temp = k;
-            }
+
+        int x = common[1] - common[0];
+        int y = common[2] - common[1];
+
+        if (x == y) {
+            answer = common[common.length - 1] + y;
+        } else {
+            answer = common[common.length - 1] * common[2] / common[1];
         }
-        if(diff==true){
-            answer = common[common.length-1]+k;
-            return answer;
-        }
-        else{
-            temp = common[1]/common[0];
-            answer = common[common.length-1]*temp;
-            return answer;
-        } 
+
+        return answer;
     }
 }
