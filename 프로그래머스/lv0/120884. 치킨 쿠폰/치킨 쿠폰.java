156157428c1f85d99d7a25d0e2ec736e01d62c0d
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-14 23:06:48 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,19 +1,11 @@
 class Solution {
-    public int solution(int c) {
-        int answer = 0;
-        int free = 0;
-        int left = 0;
-        int coupon = 0;
-        
-        // 시킨치킨 / 10 = 프리치킨 -> 시킨치킨%10 = 남은 쿠폰
-        //남은쿠폰 더하기 프리치킨 = 새로운 시작
-        while(c>9){
-            free = c/10;
-            answer += free;
-            left = c%10;
-            coupon = free+left;
-            c = coupon;
+    public int solution(int chicken) {
+        int answer = chicken/9;
+        if(chicken>1&&chicken%9==0){
+            answer--;
         }
+
+
         return answer;
     }
 }
