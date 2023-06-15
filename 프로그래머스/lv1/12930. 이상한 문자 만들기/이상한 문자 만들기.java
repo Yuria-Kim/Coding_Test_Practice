75dac01e296562d800427a45e4208a9c34660e92
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-15 23:04:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.12 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,26 +1,32 @@
 class Solution {
-  public String solution(String s) {
-      String answer = "";
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] words = s.split(" ");
 
-      char[] arr_s = s.toCharArray();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
 
-      int num=0;
-      for(int i=0; i<arr_s.length; i++){
-          // System.out.println(arr_s);
+            for (int j = 0; j < word.length(); j++) {
+                char ch = word.charAt(j);
 
-          if(arr_s[i]==' ') {
-              num=0;
-              continue;
-          }
-          if( num%2==0 && ('a'<=arr_s[i] && arr_s[i]<='z')  ){
-                  arr_s[i]-=32;
-          }
-          else if( num%2==1 && ('A'<=arr_s[i] && arr_s[i]<='Z' )){
-              arr_s[i]+=32;
-          }
-          num++;
-      }
+                if (j % 2 == 0) {
+                    answer.append(Character.toUpperCase(ch));
+                } else {
+                    answer.append(Character.toLowerCase(ch));
+                }
+            }
 
-      return String.valueOf(arr_s);
-  }
+            if (i != words.length - 1) {
+                answer.append(" ");
+            }
+        }
+
+        int lastIdx = s.length() - 1;
+        while (lastIdx >= 0 && s.charAt(lastIdx) == ' ') {
+            answer.append(" ");
+            lastIdx--;
+        }
+
+        return answer.toString();
+    }
 }
