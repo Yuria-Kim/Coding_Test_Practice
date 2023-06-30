798dc953f64f41a6bfbdd8fa8bd1bd94e7fc7256
Yuria-Kim <128604300+Yuria-Kim@users.noreply.github.com>
2023-06-30 16:21:37 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		char answer = input.charAt(0);
+		System.out.println(Integer.valueOf(answer));
+		
+	}
+}
